94449dd0d78140fd9c8af713280e55703ec416b5
ritchiejames <automatealunkal@gmail.com>
2023-09-16 08:51:54 +0400

    add print on the url for coordinate

--- sunset_sunrise.py
+++ sunset_sunrise.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import pytz
+print("use https://www.latlong.net/ to find the latitude and longitude of the desired place ")
 my_lat = float(input("enter the latitude "))
 my_long = float(input("enter the longitude "))
 
@@ -19,12 +20,6 @@ sunrise_utc  = response.json()["results"]["sunrise"]
 
 sunset_utc  = response.json()["results"]["sunset"]
 
-dubai_tz = pytz.timezone('Asia/Dubai')
-sunrise_datetime_utc = datetime.strptime(sunrise_utc, "%I:%M:%S %p")
-sunset_datetime_utc = datetime.strptime(sunset_utc, "%I:%M:%S %p")
-sunrise_datetime_gst = dubai_tz.localize(sunrise_datetime_utc).strftime('%Y-%m-%d %H:%M:%S %Z%z')
-sunset_datetime_gst = dubai_tz.localize(sunset_datetime_utc).strftime('%Y-%m-%d %H:%M:%S %Z%z')
 
+print(f"sunrise time is {sunrise_utc} UTC\n sunset time is {sunset_utc} UTC")
 
-#print(f"sunrise time is {sunrise} UTC\n sunset time is {sunset} UTC")
-#print(my_datetime_cet)
